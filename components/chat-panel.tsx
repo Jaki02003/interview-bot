6e50a708f0086d5e25ec0bf1e14888dd43e81fd0
Pranto <jabear.hossain@vivacomsolutions.com>
2024-04-30 19:25:13 +0600

    hide input

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -11,6 +11,7 @@ import { useAIState, useActions, useUIState } from 'ai/rsc'
 import type { AI } from '@/lib/chat/actions'
 import { nanoid } from 'nanoid'
 import { UserMessage } from './stocks/message'
+import { usePathname } from 'next/navigation'
 
 export interface ChatPanelProps {
   id?: string
@@ -46,8 +47,8 @@ export function ChatPanel({
     //   message: 'Set for a Golang interview adventure'
     // },
     {
-      heading: 'Ready to tackle',
-      subheading: ' Product management in the interview',
+      heading: 'Click Here',
+      subheading: ' Product Analyst Interview',
       message: 'Ready to tackle Product management in the interview'
     }
     // {
@@ -56,6 +57,9 @@ export function ChatPanel({
     //   message: 'Eager to discover Python in the interview'
     // }
   ]
+
+  const pathname = usePathname()
+  const hideInput = pathname === '/'
 
   return (
     <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
@@ -129,10 +133,12 @@ export function ChatPanel({
           </div>
         ) : null}
 
-        <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
-          <PromptForm input={input} setInput={setInput} />
-          <FooterText className="hidden sm:block" />
-        </div>
+        {!hideInput && (
+          <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
+            <PromptForm input={input} setInput={setInput} />
+            <FooterText className="hidden sm:block" />
+          </div>
+        )}
       </div>
     </div>
   )
